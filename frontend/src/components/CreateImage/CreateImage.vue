bc170f9ad7b7ae93b4a2883b1baea7fcd94ebd7f
stevenyangtw <81375443+stevenyangtw@users.noreply.github.com>
2023-06-10 11:22:13 +0800

    簡體轉繁體

--- frontend/src/components/CreateImage/CreateImage.vue
+++ frontend/src/components/CreateImage/CreateImage.vue
@@ -13,7 +13,7 @@ const emit = defineEmits<{
 
 const message = useMessage();
 
-const DEMO_KEYWORD = '骑着摩托的小猫咪，疾驰在路上，动漫场景，详细的细节。';
+const DEMO_KEYWORD = '騎著摩托的小貓咪，疾馳在路上，動漫場景，詳細的細節。';
 const keyword = ref('');
 const iframeSrc = ref('');
 const isCreating = ref(false);
@@ -25,7 +25,7 @@ const isShowModal = computed({
 
 const createImage = () => {
   if (!keyword.value) {
-    message.error('请先输入关键词');
+    message.error('請先輸入關鍵詞');
     return;
   }
   isCreating.value = true;
@@ -44,18 +44,18 @@ const useDemo = () => {
 </script>
 
 <template>
-  <NModal class="w-11/12 lg:w-[540px] select-none" v-model:show="isShowModal" :on-close="onClose" preset="card" title="图像创建">
+  <NModal class="w-11/12 lg:w-[540px] select-none" v-model:show="isShowModal" :on-close="onClose" preset="card" title="圖像創建">
     <head class="flex justify-center gap-3 px-8">
-      <NInput class="flex-1" placeholder="提示词" v-model:value="keyword" :clearable="true" @keyup.enter="createImage" maxlength="100"></NInput>
-      <NButton secondary type="info" @click="createImage" :loading="isCreating">开始创建</NButton>
+      <NInput class="flex-1" placeholder="提示詞" v-model:value="keyword" :clearable="true" @keyup.enter="createImage" maxlength="100"></NInput>
+      <NButton secondary type="info" @click="createImage" :loading="isCreating">開始創建</NButton>
     </head>
     <main class="flex justify-center items-center">
       <iframe v-if="iframeSrc" class="w-[310px] h-[350px] xl:w-[475px] xl:h-[520px] my-4" :src="iframeSrc" frameborder="0" @load="isCreating = false"></iframe>
-      <NEmpty v-else class="h-[40vh] xl:h-[60vh] flex justify-center items-center" description="暂未创建">
+      <NEmpty v-else class="h-[40vh] xl:h-[60vh] flex justify-center items-center" description="暫未創建">
         <template #extra>
-          <NButton secondary type="info" @click="useDemo">使用示例创建</NButton>
+          <NButton secondary type="info" @click="useDemo">使用示例創建</NButton>
           <div class="text-[#c2c2c2] px-2 xl:px-10">
-            <p class="text-left">提示：形容词+名词+动词+风格，描述得越详细时，效果越好。</p>
+            <p class="text-left">提示：形容詞+名詞+動詞+風格，描述得越詳細時，效果越好。</p>
             <p class="text-left">示例：{{ DEMO_KEYWORD }}</p>
           </div>
         </template>
